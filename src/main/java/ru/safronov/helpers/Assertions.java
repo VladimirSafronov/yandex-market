e74828186cd8ebaec68936793c37d77bbbe9353e
Vladimir Safronov <safron5000@mail.ru>
2023-12-13 13:04:10 +0300

    fix loading laptops with size = 0 in first page

--- src/main/java/ru/safronov/helpers/Assertions.java
+++ src/main/java/ru/safronov/helpers/Assertions.java
@@ -1,10 +1,7 @@
 package ru.safronov.helpers;
 
-import io.qameta.allure.Step;
-
 public class Assertions {
 
-  @Step("Проверяем что открылась страница с заголовком: {message}")
   public static void assertTrue(boolean condition, String message) {
     org.junit.jupiter.api.Assertions.assertTrue(condition, message);
   }
--- src/main/java/ru/safronov/helpers/DataProvider.java
+++ src/main/java/ru/safronov/helpers/DataProvider.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.params.provider.Arguments;
 
 public class DataProvider {
 
+  public static String[] companies = new String[]{"HP", "Hp", "hp", "НР", "Нр", "Lenovo", "LENOVO",
+      "lenovo"};
+
   public static Stream<Arguments> providerCheckingProduct() {
     return Stream.of(Arguments.of(
         "https://market.yandex.ru/", "Ноутбуки — купить по низкой цене на Яндекс Маркете",
-        "10000", "30000", "HP", "Lenovo", 12
+        "10000", "30000", "HP", "Lenovo", 12, companies
     ));
   }
 }
